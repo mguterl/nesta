3fa45a7d628c227862586d60b063e31df896a5c6
Michael Guterl <mguterl@gmail.com>
2010-03-21 12:04:40 -0400

    enable syntax highlighting on pages as well as summaries

--- extensions/syntax_highlighting.rb
+++ extensions/syntax_highlighting.rb
@@ -4,15 +4,6 @@ module Nesta
 
   module SyntaxHighlighting
 
-    def self.included(base)
-      base.send(:alias_method, :body_without_syntax_highlighting, :body)
-      base.send(:alias_method, :body, :body_with_syntax_highlighting)
-    end
-
-    def body_with_syntax_highlighting
-      highlight body_without_syntax_highlighting
-    end
-
     def highlight(html)
       tag = 'highlight'
       attribute = 'lang'
@@ -34,8 +25,32 @@ module Nesta
       document.to_s
     end
 
+    module BodySyntaxHighlighting
+      def self.included(base)
+        base.send(:alias_method, :body_without_syntax_highlighting, :body)
+        base.send(:alias_method, :body, :body_with_syntax_highlighting)
+      end
+
+      def body_with_syntax_highlighting
+        highlight body_without_syntax_highlighting
+      end
+    end
+
+    module PageSyntaxHighlighting
+      def self.included(base)
+        base.send(:alias_method, :to_html_without_syntax_highlighting, :to_html)
+        base.send(:alias_method, :to_html, :to_html_with_syntax_highlighting)
+      end
+
+      def to_html_with_syntax_highlighting
+        highlight to_html_without_syntax_highlighting
+      end
+    end
+
   end
 
 end
 
-Page.send(:include, Nesta::SyntaxHighlighting)
+FileModel.send(:include, Nesta::SyntaxHighlighting)
+FileModel.send(:include, Nesta::SyntaxHighlighting::PageSyntaxHighlighting)
+Page.send(:include, Nesta::SyntaxHighlighting::BodySyntaxHighlighting)
